6ad2f6b31c1cfdc1a1396bc950ab16b8362faef2
s-yeabdev <samuelyeabtsega7@gmail.com>
2026-04-26 13:10:17 +0300

    s-2

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const fs = require("fs");
 
 const PORT = 3000;
 const FILE_PATH = "./movies.json";
+function readMovies(callback) {
+  fs.readFile(FILE_PATH, "utf8", (err, data) => {
+    if (err) {
+      callback([]);
+      return;
+    }
+
+    callback(JSON.parse(data));
+  });
+}
